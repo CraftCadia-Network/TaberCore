1f851098135840bf61e3ddde396b4d488a0de86f
Sam Taber <admin@cadianetwork.com>
2019-02-12 20:39:40 -0500

    Minor change
    Forgot to add cmd to main class

--- src/com/samtaber/main/Main.java
+++ src/com/samtaber/main/Main.java
@@ -2,16 +2,22 @@ package com.samtaber.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.samtaber.main.addons.AddonHandler;
 
+
+
 public class Main extends JavaPlugin{
 
+	public static Main plugin;
+	
 	public void onEnable() {
-		
+		plugin = this;
 		AddonHandler.Setup();
 		Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Loaded TaberCore");
+		this.getCommand("loadaddon").setExecutor((CommandExecutor)new AddonHandler());
 	}
 	
 	public void onDisable() {
