36ac4fa3111883bf61ed821f9889ae856d8c7cd4
Sam Taber <admin@cadianetwork.com>
2019-02-14 20:40:40 -0500

    Code Modifications

--- src/com/samtaber/base/Base.java
+++ src/com/samtaber/base/Base.java
@@ -1,19 +1,24 @@
 package com.samtaber.base;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.tabercore.addon.main.commands.CommandVersion;
 
 public class Base extends JavaPlugin{
 	
 	public static Base instance;
 	
 	public static String serverName = "TaberGames";
-	public String serverVersion = "TaberSpigot-1.0.4-SNAPSHOT developed by Sam Taber and Josh Hanson";
-	public String serverType = "This network is running TaberProxy-3.4.0-RELEASE developed by Sam Taber and Josh Hanson";
-	public String minigameType = null;
-	public String pluginList = "This network has protected their plugins using TaberCore-ADDON_PluginProtect developed by Sam Taber";
-	public String playpackVersion = "PLAYPACKcore-0.0.1-ALPHA";	
+	public static String serverVersion = "TaberSpigot-1.0.4-SNAPSHOT developed by Sam Taber and Josh Hanson";
+	public static String serverType = "This network is running TaberProxy-3.4.0-RELEASE developed by Sam Taber and Josh Hanson";
+	public static String minigameType = null;
+	public static String pluginList = "This network has protected their plugins using TaberCore-ADDON_PluginProtect developed by Sam Taber";
+	public static String playpackVersion = "This network is running PLAYPACKcore-0.0.1-ALPHA, a system software developed by Sam Taber and Joshua Hanson alongside Lucas van Borkulo";	
 	public void onEnable() {
 		
+		this.getCommand("version").setExecutor((CommandExecutor)new CommandVersion());
+		System.out.println("loaded tabercore!");
 		instance = this;
 		
 		
