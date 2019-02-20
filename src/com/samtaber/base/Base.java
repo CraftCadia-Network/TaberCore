72d55e4b933e6bc9572ec37e6b0afb6865717009
Sam Taber <admin@cadianetwork.com>
2019-02-19 21:43:36 -0500

    Minor Changes

--- src/com/samtaber/base/Base.java
+++ src/com/samtaber/base/Base.java
@@ -8,13 +8,14 @@ public class Base extends JavaPlugin{
 	
 	public static Base instance;
 	
-	public static String globalPrefix = ChatColor.LIGHT_PURPLE + "TG> " + ChatColor.GRAY;
+	
 	public static String serverName = "TaberGames";
 	public static String serverVersion = "TaberSpigot-1.0.4-SNAPSHOT developed by Sam Taber and Josh Hanson";
 	public static String serverType = "This network is running TaberProxy-3.4.0-RELEASE developed by Sam Taber and Josh Hanson";
 	public static String minigameType = null;
-	public static String pluginList = "This network has protected their plugins using TaberCore-ADDON_PluginProtect developed by Sam Taber";
-	public static String playpackVersion = "This network is running PLAYPACKcore-0.0.1-ALPHA, a system software developed by Sam Taber and Joshua Hanson alongside Lucas van Borkulo";	
+	public static String pluginList = "This network has protected their plugins using TaberAddon_PluginProtect developed by Sam Taber";
+	public static String playpackVersion = "This network is running PLAYPACKcore-0.0.1-ALPHA, a system software developed by Sam Taber and Joshua Hanson alongside Lucas van Borkulo";
+	public static String globalPrefix = ChatColor.LIGHT_PURPLE + "TG> " + ChatColor.GRAY;
 	public void onEnable() {
 		
 		
